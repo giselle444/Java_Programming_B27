34b9fed7895f733954f227b41bd17814e7b455cc
Giselle <2014giselle@gmail.com>
2022-08-19 14:35:34 -0400

    day 38 access modifiers

---
+++ src/day38_encapsulation/Friday.java
@@ -0,0 +1,15 @@
+package day38_encapsulation;
+
+ class Friday { //  .java file should be the same as the public class
+    //only one public class needed in a file that matches the name of the file
+
+} //Friday class ends after these brackets
+
+class Monday{
+
+}
+
+
+
+
+
